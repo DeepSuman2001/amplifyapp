cf7bb0cffda5dacdc7e06d516269691978c456f7
Deep Suman <deepsuman2001@gmail.com>
2023-04-24 10:47:49 +0530

    new changes made in heading

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Deep Project</h1>
+        <h1>Deeps amplify Project</h1>
       </header>
     </div>
   );
